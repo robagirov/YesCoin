cb0d37c6e53b98d7c09b8c056ffeb7b1f5f05af1
Rovshan Bagirov <ro.bagirov@icloud.com>
2024-02-13 21:13:15 +0300

    Test autoprefixer

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
+import autoprefixer from "autoprefixer";
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths(), svgr()],
+  css: {
+    postcss: {
+      plugins: [autoprefixer],
+    },
+  },
 });
