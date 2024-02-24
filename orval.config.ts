f1b918f8f231a2337307278d4dfbd63dbd3cb089
Andrei Albov <61917842+AndrewScottPH@users.noreply.github.com>
2024-02-24 14:09:54 +0300

    generate api

---
+++ orval.config.ts
@@ -0,0 +1,16 @@
+module.exports = {
+  yesCoin: {
+    output: {
+      mode: "split",
+      workspace: "./src/shared/openApi/",
+      target: "index.ts",
+      schemas: "model",
+      client: "react-query",
+      tsconfig: true,
+      prettier: true,
+    },
+    input: {
+      target: "http://yestoken.space/openapi.json",
+    },
+  },
+};
