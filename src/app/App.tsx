746fcca426693c39e699036ead4f47fda420e797
Andrew <andrewscottph@gmail.com>
2024-02-26 14:15:43 +0300

    extract router from App

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,51 +1,15 @@
-import './styles/index.scss'
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import {
-  MainBoard,
-  TradeMarket,
-  CryptoHolder,
-  YourFriends,
-  GoldLeague,
-  FarmCoins,
-  GameBoosts,
-  PartyKings,
-  YesCoin,
-  EnterSquad,
-  SquadName,
-  JoinSquad,
-  SquadWork,
-  MajorDigits,
-} from 'pages'
-import { ROUTES } from 'shared/consts'
-import { QueryProvider, TelegramProvider } from 'app/providers'
 import eruda from 'eruda'
+import { QueryProvider, TelegramProvider, RouterProvider } from './providers'
+import './styles/index.scss'
 
 function App() {
   eruda.init()
 
   return (
     <QueryProvider>
-      <Router>
-        <TelegramProvider>
-          <Routes>
-            <Route path={ROUTES.MAIN_BOARD} element={<MainBoard />} />
-            <Route path={ROUTES.CRYPTO_HOLDER} element={<CryptoHolder />} />
-            <Route path={ROUTES.TRADE_MARKET} element={<TradeMarket />} />
-            <Route path={ROUTES.GOLD_LEAGUE} element={<GoldLeague />} />
-            <Route path={ROUTES.YOUR_FRIENDS} element={<YourFriends />} />
-            <Route path={ROUTES.FARM_COINS} element={<FarmCoins />} />
-            <Route path={ROUTES.GAME_BOOST} element={<GameBoosts />} />
-            <Route path={ROUTES.PARTY_KINGS} element={<PartyKings />} />
-            <Route path={ROUTES.YES_COIN} element={<YesCoin />} />
-            <Route path={ROUTES.ENTER_SQUAD} element={<EnterSquad />} />
-            <Route path={ROUTES.SQUAD_NAME} element={<SquadName />} />
-            <Route path={ROUTES.JOIN_SQUAD} element={<JoinSquad />} />
-            <Route path={ROUTES.SQUAD_WORK} element={<SquadWork />} />
-            <Route path={ROUTES.MAJOR_DIGITS} element={<MajorDigits />} />
-          </Routes>
-        </TelegramProvider>
-      </Router>
+      <RouterProvider>
+        <TelegramProvider />
+      </RouterProvider>
     </QueryProvider>
   )
 }
