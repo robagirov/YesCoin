fb2877afb6f3db54e6592797d2c3cd7546ff05e8
Andrew <andrewscottph@gmail.com>
2024-02-26 11:55:24 +0300

    add stylelint config

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+export default {
+  extends: ['stylelint-config-standard-scss'],
+  rules: {
+    'selector-class-pattern': null,
+    'scss/dollar-variable-pattern': null,
+    'scss/comment-no-empty': null,
+    'property-no-vendor-prefix': null,
+  },
+}
