a95c172a7dc7f9c8adedf30344dcf329fb21d390
Andrew <andrewscottph@gmail.com>
2024-02-26 12:01:51 +0300

    stylelint configure

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,8 @@
 const eslintCommand = (filenames) => `eslint --debug ${filenames.join(' ')} --fix`
 const formatCommand = (filenames) => `prettier --write ${filenames.join(' ')}`
 const gitCommand = (filenames) => `git add ${filenames.join(' ')}`
-const stylelintCommand = 'stylelint --allow-empty-input "**/*.{css,scss}" --fix'
+// const stylelintCommand = 'stylelint --allow-empty-input "**/*.{css,scss}" --fix'
+const stylelintCommand = (filenames) => `stylelint  --fix  ${filenames.join(' ')}`
 
 module.exports = {
   '*.{cjs,js,jsx,ts,tsx}': [formatCommand, eslintCommand, gitCommand],
