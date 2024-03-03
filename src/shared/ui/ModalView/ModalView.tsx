e629fdff0b1f72f8d77e8c346f9911e6eeee4f7e
Rovshan Bagirov <ro.bagirov@icloud.com>
2024-03-03 22:28:22 +0300

    fix(YC-55): fix modal opacity and animation transform

--- src/shared/ui/ModalView/ModalView.tsx
+++ src/shared/ui/ModalView/ModalView.tsx
@@ -3,13 +3,13 @@ import type { ReactNode } from 'react'
 import { createPortal } from 'react-dom'
 import styles from './styles.module.scss'
 
-interface ModalViewProps {
+interface Props {
   isOpen: boolean
   className?: string
   children: ReactNode
 }
 
-export const ModalView = ({ className, children, isOpen }: ModalViewProps) =>
+export const ModalView = ({ className, children, isOpen }: Props) =>
   createPortal(
     <div className={clsx(styles.dialog, { [styles.open]: isOpen }, className)}>{children}</div>,
     document.getElementById('portal')!,
