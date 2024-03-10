116507a84683d286dff2cd3b7e6c8e9da860c88a
Rovshan Bagirov <ro.bagirov@icloud.com>
2024-03-10 13:33:42 +0300

    feat(YC-59): squad page redesign

--- src/features/top-list/ui/TopList.tsx
+++ src/features/top-list/ui/TopList.tsx
@@ -1,10 +1,15 @@
 import { ListBlock, NumberedItem } from 'shared/ui'
 import { mockData } from 'features/top-list/model/mock.ts'
+import clsx from 'clsx'
 import styles from './styles.module.scss'
 
-export function TopList() {
+interface TopListProps {
+  className?: string
+}
+
+export function TopList({ className }: TopListProps) {
   return (
-    <ListBlock className={styles.topChartList}>
+    <ListBlock className={clsx(styles.topChartList, className)}>
       {mockData.map(({ friends, name, avatar }, index) => (
         <NumberedItem
           key={name}
--- src/pages/JoinSquad/JoinSquad.tsx
+++ src/pages/JoinSquad/JoinSquad.tsx
@@ -1,5 +1,6 @@
 import { ActionButton, LayoutContent } from 'shared/ui'
 import { Typography } from 'shared/ui/Typography'
+import { Picture } from 'shared/ui/Picture'
 import styles from './styles.module.scss'
 
 export function JoinSquad() {
@@ -11,12 +12,13 @@ export function JoinSquad() {
         <div className="relative w-24 h-24 rounded-full bg-[rgb(243,243,245,0.1)] -left-6" />
       </div>
 
-      <Typography variant="h2" className="mb-72">
+      <Typography variant="h2" className="mb-72" align="center">
         Присоединиться к Арнольд Криптовалютный Squad
       </Typography>
 
       <div className={styles.additional}>
         <div>Твой фарминг монет увеличивает счет сквада в лидерборде</div>
+
         <div>Скажи Yes новым друзьям и совместному фармингу</div>
       </div>
 
