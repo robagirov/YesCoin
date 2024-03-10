8823b2f2376d236ad680dcef605e6eb85be9c240
Andrew <andrewscottph@gmail.com>
2024-03-10 14:16:17 +0300

    merge

--- src/features/friends-list/ui/FriendItem.tsx
+++ src/features/friends-list/ui/FriendItem.tsx
@@ -1,34 +1,52 @@
 import { Picture } from 'shared/ui/Picture'
 import { Typography } from 'shared/ui/Typography'
-import { GamerLevel } from 'shared/ui/GamerLevel'
-import { USER_PRO_LEVEL } from 'shared/consts/user-levels.ts'
+// import { GamerLevel } from 'shared/ui/GamerLevel'
+// import { USER_PRO_LEVEL } from 'shared/consts/user-levels.ts'
+import { Link } from 'react-router-dom'
+import clsx from 'clsx'
 import styles from './styles.module.scss'
 
 interface FriendItemProps {
   avatar?: string
-  name: string
-  level: 'Pro' | 'Middle'
-  bonus: number
+  telegram_id: number
+  username: string
+  // level?: 'Pro' | 'Middle'
+  // bonus?: number
+  asLink: boolean
 }
 
-export function FriendItem({ avatar, name, level, bonus }: FriendItemProps) {
-  const bonusColor = level === USER_PRO_LEVEL ? 'gold' : 'gray'
+// TODO: нет половины данных
+
+export function FriendItem({
+  avatar,
+  username,
+  telegram_id: id,
+  // level = 'Middle',
+  // bonus,
+  asLink,
+}: FriendItemProps) {
+  const as = { element: asLink ? Link : 'div' }
+  // const bonusColor = level === USER_PRO_LEVEL ? 'gold' : 'gray'
+
+  const className = clsx(styles.wrapper, {
+    [styles.wrapper_noArrow]: !asLink,
+  })
 
   return (
-    <div className={styles.wrapper}>
+    <as.element to={id.toString()} className={className}>
       <Picture type="round" src={avatar} />
 
       <div className={styles.info}>
         <div className={styles.inner}>
-          <Typography variant="h6">{name}</Typography>
+          <Typography variant="h6">{username}</Typography>
 
-          <GamerLevel level={level} />
+          {/* <GamerLevel level={level} /> */}
         </div>
 
-        <Typography variant="h6" color={bonusColor}>
-          +{bonus}k
-        </Typography>
+        {/* <Typography variant="h6" color={bonusColor}> */}
+        {/*  +{bonus}k */}
+        {/* </Typography> */}
       </div>
-    </div>
+    </as.element>
   )
 }
