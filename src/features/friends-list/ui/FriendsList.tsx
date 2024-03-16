774c3ec2f16a48623e4d650cca2c58d96343f583
Rovshan Bagirov <ro.bagirov@icloud.com>
2024-03-17 00:44:54 +0300

    build(YC-63): add cat to empty friends list

--- src/features/friends-list/ui/FriendsList.tsx
+++ src/features/friends-list/ui/FriendsList.tsx
@@ -4,8 +4,12 @@ import { useParams } from 'react-router-dom'
 import { useTelegramUserId } from 'entities/Telegram'
 import { useGetFriendsSuspense } from 'shared/openApi'
 import { Suspense } from 'react'
+import Lottie from 'react-lottie'
+import { Typography } from 'shared/ui/Typography'
 import { FriendItem } from './FriendItem'
 import styles from './styles.module.scss'
+
+import animationData from '../../../../assets/lotties/cat-no-friends.json'
 
 interface FriendsListProps {
   className?: string
@@ -27,7 +31,21 @@ function ItemsList() {
   const { data, isError } = useGetFriendsSuspense(Number(id) || userId)
 
   // TODO: отрисовать состояние пустого списка
-  if (!data?.length || isError) return <p>У вас нет друзей</p>
+  if (!data?.length || isError)
+    return (
+      <div className={styles.empty}>
+        <Typography variant="h5">Вы никого не пригласили</Typography>
+
+        <Lottie
+          options={{
+            loop: true,
+            autoplay: true,
+            animationData,
+          }}
+          style={{ width: '15rem', height: '15rem' }}
+        />
+      </div>
+    )
 
   return data.map((user) => <FriendItem asLink={!id} {...user} key={user.telegram_id} />)
 }
