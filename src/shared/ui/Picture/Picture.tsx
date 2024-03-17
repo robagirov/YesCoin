b0d3e411e82e7ec7fa2a54969952009f36dcdb0f
Rovshan Bagirov <ro.bagirov@icloud.com>
2024-03-17 17:10:42 +0300

    refactor(YC-33): delete tailwind styles

--- src/shared/ui/Picture/Picture.tsx
+++ src/shared/ui/Picture/Picture.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import styles from './styles.module.scss'
 
 interface PictureProps {
@@ -8,11 +9,9 @@ interface PictureProps {
 }
 
 export function Picture({ src, size = 'medium', type, className }: PictureProps) {
-  const classList = [styles.container, styles[size], styles[type], className]
-    .filter(Boolean)
-    .join(' ')
-
   return (
-    <div className={classList}>{src && <img src={src} alt="" className={styles.picture} />}</div>
+    <div className={clsx(styles.container, styles[size], styles[type], className)}>
+      {src && <img src={src} alt="" className={styles.picture} />}
+    </div>
   )
 }
