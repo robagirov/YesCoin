685a7a56437b63aa38bde1e0e88e2df50e60c90e
aintnomoreloyalty <nikonikotrall@gmail.com>
2024-03-20 20:21:41 +0300

    fix: api url

--- src/app/MainBoard/MainBoard.tsx
+++ src/app/MainBoard/MainBoard.tsx
@@ -11,7 +11,7 @@ export function MainBoard() {
 
 
   const initDataRaw = useInitDataRaw();
-  const { data } = useQuery({ queryKey: ['player'], queryFn: () => fetch('https://yescoin.space/players/getById', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ tgWebData: initDataRaw }) }).then(r => r.json()) })
+  const { data } = useQuery({ queryKey: ['player'], queryFn: () => fetch('https://yestoken.space/players/getById', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify({ tgWebData: initDataRaw }) }).then(r => r.json()) })
 
   console.log('initDataRaw: ', initDataRaw);
 
