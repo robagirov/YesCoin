c15258ae78a52220e24e0c5300701c75384228c7
aintnomoreloyalty <nikonikotrall@gmail.com>
2024-03-20 14:03:41 +0300

    twa async init

--- src/app/Root.tsx
+++ src/app/Root.tsx
@@ -1,7 +1,28 @@
 import { DisplayGate, SDKProvider } from '@tma.js/sdk-react'
 import App from './App.tsx'
-import { useEffect } from 'react'
+import { FC, useEffect } from 'react'
 import { setDebug } from '@tma.js/sdk'
+
+const Err: FC<{ error: unknown }> = ({ error }) => {
+  return (
+    <div>
+      <p>An error occurred while initializing the SDK</p>
+      <blockquote>
+        <code>
+          {error instanceof Error
+            ? error.message
+            : JSON.stringify(error)}
+        </code>
+      </blockquote>
+    </div>
+  );
+};
+
+const Loading: FC = () => {
+  return (
+    <div>Application is loading</div>
+  );
+};
 
 export function Root() {
 
@@ -10,8 +31,10 @@ export function Root() {
   }, []);
 
   return (
-    <SDKProvider>
-    <App/>
+    <SDKProvider options={{ acceptCustomStyles: true, cssVars: true, complete: true }}>
+      <DisplayGate error={Err} loading={Loading} initial={Loading}>
+        <App />
+      </DisplayGate>
     </SDKProvider>
   );
 }
